7a86e7321e181554621a9837857576f9f5f55ad6
JayasudhanG <115360522+JayasudhanG@users.noreply.github.com>
2023-05-23 19:08:54 +0530

    Add files via upload

---
+++ NumPy.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+ 
+
+
+# In[3]:
+
+
+ag_list=[12,34,78,98]
+age_array=np.array(ag_list)
+print(age_array)
+
+
+# In[4]:
+
+
+np.random.rand(2,3)
+
+
+# In[5]:
+
+
+np.random.rand(2,3)
+
+
+# In[6]:
+
+
+np.random.rand(2,3)
+
+
+# In[7]:
+
+
+np.random.rand(2,3)
+
+
+# In[8]:
+
+
+np.random.rand(2,3)
+
+
+# In[11]:
+
+
+np.random.random(size=20)
+
+
+# In[12]:
+
+
+np.random.randint(1,101,100)
+
+
+# In[15]:
+
+
+np.random.randint(1,10)
+
+
+# In[16]:
+
+
+np.random.random(size =500)
+
+
+# In[19]:
+
+
+jaya_mass=np.arange(start=10,stop=14,step=2)
+print(jaya_mass)
+
+
+# In[20]:
+
+
+np.arange(1,10)
+
+
+# In[21]:
+
+
+np.arange(10)
+
+
+# In[22]:
+
+
+np.arange(start=5,stop=50,step=5)
+
+
+# In[24]:
+
+
+np.arange(start=100,stop=1,step=-1)
+
+
+# In[26]:
+
+
+np.linspace(0,1,5)
+
+
+# In[30]:
+
+
+np.zeros(10,int)
+
+
+# In[32]:
+
+
+np.ones(100,int)
+
+
+# In[35]:
+
+
+np.empty((3,3) , dtype=int)
+
+
+# In[42]:
+
+
+np.full((3,3) ,23)
+
+
+# In[52]:
+
+
+np.arange(1,21)
+
+
+# In[58]:
+
+
+man1=np.arange(1,21)
+man1.reshape(10,2)
+
+
+# In[63]:
+
+
+print(sample_array[4])
+
